0b50ec1cf9a747c24406e1228e0c37ab427a3d72
cemcoe <chemxcode@gmail.com>
2021-12-16 03:04:07 +0000

    Deploying to gh-pages from  @ 07c7648f125db7867a1f75dd7af86b176de22e87 🚀

---
+++ precache-manifest.cc0a09d82b9db337f4e14900a03c2af7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cbcc08ffb2437e707d",
+    "url": "/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "88740a8565f68db5e9b585f36aecc775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8537bf0e5fd17ce4918",
+    "url": "/js/about.f731f4dc.js"
+  },
+  {
+    "revision": "33cbcc08ffb2437e707d",
+    "url": "/js/app.00631a63.js"
+  },
+  {
+    "revision": "a76cfa12ec3f2b1f10ef",
+    "url": "/js/chunk-vendors.7046bc9a.js"
+  },
+  {
+    "revision": "66d2324bb47b20027a7780266dcdcc8f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
