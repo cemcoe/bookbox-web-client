147728c7f89f38949703502eea8401c6e5cd3f49
cemcoe <chemxcode@gmail.com>
2022-01-09 09:10:26 +0800

    feat: import vant@3

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="login">
     login
+    <van-button type="primary">登录</van-button>
   </div>
 </template>
 
 <script setup>
+// <script setup> 范围里的值也能被直接作为自定义组件的标签名使用
+
+
 
 
 
