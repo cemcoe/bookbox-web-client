2a8ed3cebb1f8f961da2a6994f166f8257ea30b0
cemcoe <chemxcode@gmail.com>
2022-01-25 19:19:10 +0800

    component(NavBar): leftArrow and title attr)

--- src/components/content/NavBar/NavBar.vue
+++ src/components/content/NavBar/NavBar.vue
@@ -1,18 +1,36 @@
 <template>
+  <!-- <nav-bar left-arrow title="title"></nav-bar> -->
   <div class="nav-bar">
-      <div class="left">
-        <slot name="left"></slot>
-      </div>
-      <div class="center">
-        <slot name="center"></slot>
-      </div>
-      <div class="right">
-        <slot name="right"></slot>
-      </div>
+    <div class="left">
+      <!-- {{ props.leftArrow }} -->
+      <!-- 你如果给我了值那么，left就展示的是返回的功能, 下面的同理 -->
+      <slot name="left" v-if="!props.leftArrow"></slot>
+      <div class="back" @click="history.back()" v-else>回</div>
     </div>
+    <div class="center">
+      <slot v-if="!props.title" name="center"></slot>
+      <div class="title" v-else>{{ props.title }}</div>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  // 如果有值就不是插槽模式
+  leftArrow: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: '',
+  }
+})
 
 </script>
 
