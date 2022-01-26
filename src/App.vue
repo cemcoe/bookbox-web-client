375905f829de3bbe7499b7b89b5d024a35a2bf14
cemcoe <chemxcode@gmail.com>
2022-01-26 14:19:12 +0800

    chore: import vuerouter

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <RouterView></RouterView>
 </template>
 
 <style>
