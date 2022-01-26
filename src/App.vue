314422fee48f59df4e888ffa372860d8b01d64f9
cemcoe <chemxcode@gmail.com>
2022-01-26 10:36:34 +0800

    chore(vite): webpack to vite

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <router-view />
-  <main-tab-bar v-if="$route.meta.showTabbar"></main-tab-bar>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
-
-<script setup>
-import MainTabBar from 'components/content/MainTabBar/MainTabBar'
-
-
-</script>
