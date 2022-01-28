cd8c8fbdea88493189caa4d9aca47dd597afdd30
cemcoe <chemxcode@gmail.com>
2022-01-28 13:13:10 +0800

    feat: Post Detail

--- src/network/post.ts
+++ src/network/post.ts
@@ -6,3 +6,28 @@ export function getPostList() {
     { method: "GET", }
   )
 }
+
+// 创建文章
+export function createPost(post) {
+  return request(
+    '/v1/post',
+    { method: "POST", data: post },
+  )
+}
+
+// 获取文章详情
+export function getPostDetailAPI(id) {
+  return request(
+    `/v1/post/${id}`,
+    { method: "GET" }
+  )
+}
+
+// 更新文章
+export function updatePostApi(id, post) {
+  console.log(id, post, 'resuest')
+  return request(
+    `/v1/post/${id}`,
+    { method: "PUT", data: post }
+  )
+}
--- src/views/Post/Post.vue
+++ src/views/Post/Post.vue
@@ -1,11 +1,62 @@
-<template lang="">
-  <div>
-    Post
+<template>
+  <div class="post">
+    <post-nav-bar />
+    <div class="post-box">
+      <h1 class="title">{{ post.title }}</h1>
+      <div class="author">
+        <p>{{ post.authorName }}</p>
+      </div>
+      <article>
+        <div class="post-content" v-html="previewContent"></div>
+      </article>
+    </div>
   </div>
 </template>
-<script setup lang="ts">
 
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { getPostDetailAPI } from '../../network/post';
+import PostNavBar from './components/PostNavBar.vue'
+import { marked } from "marked";
+
+
+const route = useRoute()
+const post = ref({})
+let previewContent = ref('')
+
+const getPostDetail = async () => {
+  const result = await getPostDetailAPI(route.params.id)
+  const { status } = result
+  if (status === 200) {
+    post.value = result.data.post
+    previewContent.value = marked.parse(result.data.post.content)
+  }
+}
+
+onMounted(getPostDetail())
 </script>
+
+
 <style scoped>
-  
+.title {
+  color: #000;
+  font-size: 20px;
+  padding-top: 20px;
+}
+
+.author {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.post-box {
+  padding: 10px;
+}
+
+.post-content {
+  line-height: 26px;
+}
 </style>
+
